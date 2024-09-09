95c2da44fe1de07a12fc69965b0c03e0c86a0953
Piyush khare <piyushkharejhs@gmail.com>
2024-09-09 13:13:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
+
     eslint : {
         ignoreDuringBuilds : true,
       }
-=======
-      eslint : {
-      ignoreDuringBuilds : true,
-    }
 
-      
->>>>>>> 2309047996493b67608dbdb11dfc6c24a605041b
+
 };
 
 export default nextConfig;
